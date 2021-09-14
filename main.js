8817cd68448c16fc6f2b98f0ee0d3bbb807271e3
AarshRam <74356212+AarshRam@users.noreply.github.com>
2021-09-14 16:58:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 song1 = "";
 song2 = "";
 
+rightWristY = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY= 0;
+
 function setup() {
     canvas = createCanvas(600 , 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet( video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        rightWristY = results[0].pose.rightWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        console.log("RightWrist X = " + rightWristX + " , the RightWrist Y = " + rightWristY);
+        
+        leftWristY = results[0].pose.leftWrist.y;
+        leftWristX = results[0].pose.leftWrist.x;
+
+        console.log("The LeftWrist X = " + leftWristX + " , LeftWrist Y = " + leftWristY);
+    }
 }
 
 function draw(){
@@ -19,4 +46,6 @@ function preload(){
 
 function play() {
     song1.play();
+    song1.setVolume(0.8);
+    song1.rate(1);
 }
